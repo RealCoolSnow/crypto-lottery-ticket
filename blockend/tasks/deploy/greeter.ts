bb34ba2f9d05739ae83a18b56d6339da00c07544
CoolSnow <coolsnow2020@gmail.com>
2022-06-06 16:43:46 +0800

    fix

--- blockend/tasks/deploy/greeter.ts
+++ blockend/tasks/deploy/greeter.ts
@@ -2,8 +2,7 @@ import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { task } from "hardhat/config";
 import type { TaskArguments } from "hardhat/types";
 
-import type { Greeter } from "../../src/types/Greeter";
-import type { Greeter__factory } from "../../src/types/factories/Greeter__factory";
+import { Greeter, Greeter__factory } from "../../src/types";
 
 task("deploy:Greeter")
   .addParam("greeting", "Say hello, be nice")
